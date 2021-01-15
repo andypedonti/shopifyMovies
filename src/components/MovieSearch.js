33c999ffde6d00bab88e19c9de822502a4f4060a
troutbum <64603749+andy.pedonti@users.noreply.github.com>
2021-01-14 22:31:57 -0500

    building the search bar

---
+++ src/components/MovieSearch.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+
+const movieSearch = (props) => {
+    return (
+        <form>
+            <div className="form-group">
+                <label htmlFor="search">Search:</label>
+                <input
+                    onChange={props.handleInputChange}
+                    value={props.value}
+                    name="search"
+                    type="text"
+                    className="form-control"
+                    value="props.value"
+                    placeholder="Search For a Movie"
+                    id="search"
+
+                />
+                <button
+                    onClick={props.handleFormSubmit}
+                    className="btn btn-primary">
+                    Search
+            </button>
+            </div>
+        </form>
+    )
+}
+
+export default movieSearch;
