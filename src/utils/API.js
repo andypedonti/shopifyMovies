99b6dd830bc9a52ca6fd32d9d2360b0d723f81c0
troutbum <64603749+andy.pedonti@users.noreply.github.com>
2021-01-14 22:20:16 -0500

    axios and api key added

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,4 +1,10 @@
 import axios from "axios";
 
-const BASERUL = "";
-const APIKEY = "2d79ebc8";
+const BASERUL = "https://www.omdbapi.com/?t=";
+const APIKEY = "&apikey=2d79ebc8";
+
+export default {
+    search: function (query) {
+        return axios.get(BASEURL + query + APIKEY);
+    }
+};
